e59f28f7e25dec90513e57f588ce6cc8a2b8e188
Grant Wuerker <gwuerker@gmail.com>
2019-02-16 11:54:02 -0700

    clearing time refactored

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,9 +6,10 @@ export function handleAuctionCleared(event: AuctionCleared): void {
   // for ease of use
   let params = event.params
 
-  // get the final price of the tokens (sell / buy)
+  // get the final price of the tokens (sell / buy) and the clearing time
   let dutchExchange = DutchExchange.bind(event.address)
   let price = dutchExchange.getPriceInPastAuction(params.sellToken, params.buyToken, params.auctionIndex)
+  let clearingTime = dutchExchange.getClearingTime(params.sellToken, params.buyToken, params.auctionIndex)
 
   // auction should already exist, but if not, create a new one
   let auctionId = auctionId(params.sellToken, params.buyToken, params.auctionIndex)
@@ -27,7 +28,7 @@ export function handleAuctionCleared(event: AuctionCleared): void {
   auction.priceNum = price.value0
   auction.priceDen = price.value1
   auction.cleared = true
-  auction.clearingTime = dutchExchange.getClearingTime(params.sellToken, params.buyToken, params.auctionIndex)
+  auction.clearingTime = clearingTime
   auction.save()
 }
 
